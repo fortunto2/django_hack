adb4d43214492c196645b4d44a75e600055db332
fortunto2 <info@3dstr.ru>
2017-10-19 14:23:02 +0500

    Добавили в модель комментарии

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    text2 = models.TextField(blank=True)
+    # image = models.ImageField()
+
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -17,3 +20,24 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+
+class Comment(models.Model):
+    author = models.ForeignKey('auth.User')
+    text = models.TextField()
+
+    post = models.ForeignKey('Post', blank=True, null=True)
+
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text
